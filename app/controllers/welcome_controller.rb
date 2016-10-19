bcbdbcd592947237985a58c328a625d4552799cb
JoannaParker1 <j.parker2099@live.com>
2016-10-18 22:37:42 -0700

    Initialize repository and first co

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homestate = 'Arizona'
+  	@countries = ['Canada', 'United States', 'Mexico']
+  	@images = ['Victoria.jpeg', 'US.jpeg', 'Mexico.jpeg']
+  end
+
+  def about
+  	@color = params[:color]
+  	@size = params[:size]
+  end
+end
